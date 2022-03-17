2d9f300bbea567aa320b8972ffe36be8a722bd45
LowEntropyBody <jszyyx@163.com>
2022-03-17 13:42:16 +0800

    use default 8080

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const createRequest = require('./index').createRequest
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.EA_PORT || 8081
+const port = process.env.EA_PORT || 8080
 
 app.use(bodyParser.json())
 
